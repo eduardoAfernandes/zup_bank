c61bebb1121ff3195a9df5810533f82e3dbebcdf
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2020-12-29 15:28:43 -0300

    Criacao do Projeto

---
+++ src/test/java/com/zup_it/zup_bank/ZupBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup_it.zup_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
