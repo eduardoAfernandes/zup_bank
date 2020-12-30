ffa14462b4eb3d915093c5b9a79c3ebf68e2b19b
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2020-12-30 09:15:22 -0300

    Configuração do banco de dados em memória H2

--- src/main/java/com/zup_it/zup_bank/entities/Customer.java
+++ src/main/java/com/zup_it/zup_bank/entities/Customer.java
@@ -2,8 +2,13 @@ package com.zup_it.zup_bank.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Entity
 public class Customer implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -11,9 +16,15 @@ public class Customer implements Serializable{
 	
 	// Criacao dos atributos que o cliente tera
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
+	
+	@Column(unique = true)
 	private String cpf;
 	private Instant dataNascimento;
 	
